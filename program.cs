56f73c435b689683dcfe29d9d79c480ccfc5231e
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2023-02-16 20:08:23 +0100

    Initial commit
    Aquí lo tienen

---
+++ program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace calculadora_c_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 0;
+        Console.WriteLine("Para sumar 1 pon Y,pon reset para volver a cero, si quieres salir pulsa otra cosa");
+        do {
+            Console.WriteLine("Has puesto Y "+ a++ + " Veces");
+            
+        }while (Console.ReadLine().ToUpper() == "Y");
+        Console.WriteLine("Adios! ");
+        Console.ReadKey();
+
+
+    }
+}
